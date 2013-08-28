e8de1a31558b000c97e4ac2cdf61b60c083c9985
Sajit Mathew Kunnumkal <skunnumkal@amplify.com>
2013-08-28 18:33:13 -0400

    jphm

--- project/Build.scala
+++ project/Build.scala
@@ -14,7 +14,9 @@ object ApplicationBuild extends Build {
     anorm,
     "mysql" % "mysql-connector-java" % "5.1.18",
     "com.google.code.gson" % "gson" % "1.7.1",
-    "org.apache.commons" % "commons-io" % "1.3.2"
+    "org.apache.commons" % "commons-io" % "1.3.2",
+    "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
+
   )
 
   // Add your own project settings here
