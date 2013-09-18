8ed7131d4734c97288bf9a2a6012b45c4c928737
Sajit Mathew Kunnumkal <skunnumkal@amplify.com>
2013-09-18 12:09:57 -0400

    error handling matching

--- app/services/MatchingService.scala
+++ app/services/MatchingService.scala
@@ -11,8 +11,21 @@ object MatchingService {
   val strategy:MatchingStrategy = new MongoMatchingStrategy()
   def getMatches(usersCommute:MongoUserCommute,otherCommutes:List[MongoUserCommute]):List[MongoUserCommute] = {
       val matches:List[MongoUserCommute] = otherCommutes.filter(commute => {
-        !commute.id.equals(usersCommute.id)   && !commute.userId.equals(usersCommute.userId) &&
-          strategy.isMatch(commute,usersCommute)
+        val differentCommute:Boolean = !commute.id.equals(usersCommute.id)   && !commute.userId.equals(usersCommute.userId)
+        if(differentCommute){
+          try{
+            strategy.isMatch(commute,usersCommute)
+          } catch {
+            case re:RuntimeException => {
+              //TODO flag such commutes
+              println("Had an issue with address" + re.getMessage)
+              false
+            }
+          }
+
+        }
+        else false
+
 
       })
       matches
